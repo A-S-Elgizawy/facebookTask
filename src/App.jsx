3ca17c61124b0001de4a78a210e8331d9ef096f6
Ahmed Said Elgizawy <elgizawy574@gmail.com>
2025-12-31 23:52:47 +0200

    fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route ,HashRouter} from 'react-router-dom'
 import Home from './pages/Home'
 import Account from './pages/createAccount'
 import Login from './pages/Login'
 
 function App() {
   return (
-      <BrowserRouter>
+      <HashRouter>
       <Routes>
         <Route path="/Home" element={<Home />} />
         <Route path="/" element={<Account />} />
         <Route path="/Login" element={<Login />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
